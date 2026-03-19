02178d54d3c53db586975b4d2119209bcd13c062
Jarpla Vijay Kumar <jarplavijaykumar426@gmail.com>
2026-03-20 00:55:43 +0530

    Document replace_macros function parameters
    Added function documentation for replace_macros.

--- macro_handler.h
+++ macro_handler.h
@@ -1,1 +1,5 @@
-void replace_macros(const char *, const char *);
+// Function to replace all macros in a source file
+// Parameters:
+// 1) const char *input_file  -> Name of the file to read source code from
+// 2) const char *output_file -> Name of the file to write processed code after macro replacement
+void replace_macros(const char *input_file, const char *output_file);
